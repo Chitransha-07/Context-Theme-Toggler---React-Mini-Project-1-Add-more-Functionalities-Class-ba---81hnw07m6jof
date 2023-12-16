95b6a3adf998f2206fb09654d292187066e4c136
Chitransha-07 <141350081+Chitransha-07@users.noreply.github.com>
2023-12-16 22:19:23 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,27 +1,19 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 const ThemeContext = createContext();
 
 const ThemeProvider = (props) => {
-  const [theme, setTheme] = useState('light');
+  const [globalTheme, setGlobalTheme] = useState('light');
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  const toggleGlobalTheme = () => {
+    setGlobalTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ globalTheme, toggleGlobalTheme }}>
       {props.children}
     </ThemeContext.Provider>
   );
 };
 
-const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider');
-  }
-  return context;
-};
-
-export { ThemeProvider, useTheme, ThemeContext };
+export { ThemeProvider, ThemeContext };
